3c492d683aae034b9e7d274226f0c73eda74e7ed
Pradeepchunji <67774847+pradeepchunji58@users.noreply.github.com>
2022-09-11 15:54:44 +0530

    provider

---
+++ Aws provider.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  access_key                  = "mock_access_key"
+  region                      = "us-east-1"
+  secret_key                  = "mock_secret_key"
+}
+
+ # s3_force_path_style         = true
